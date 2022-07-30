d2f15ba5a34d14b7253672b234fddad7044b7436
SEB534542 <shj.vandermeulen@gmail.com>
2022-07-30 11:23:44 +0200

    x

---
+++ conversion.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+// Conv contains the item conversion from 100 gram to a cup
+var convTable = map[string]float64{
+	"Quinoa": 0.555,
+}
+
+const (
+	gram = "gr"
+	cup  = "cup"
+	ml   = "ml"
+	tbsp = "el"
+	tsp  = "tl"
+)
+
+var (
+	cupToMilliliter = 0.2841306 // fixed cup to milliliter ratio
+	cupToTbsp       = 16.0      // fixed cup to tablespoon ration
+	cupToTsp        = 48.0      // fixed cup to teaspoon ratio
+)
+
+/*Uoms takes an ingredient and return the amount for two pre-determined
+alternative Unit of Measurements combined into one string.*/
+func (i Ingr) uoms() string {
+	switch i.Unit {
+	case gram:
+		c := round(gramToCup(i.Item, i.Amount))
+		m := round(c * cupToMilliliter)
+		return fmt.Sprintf("%v %v / %v %v", c, cup, m, ml)
+	case cup:
+		g := round(cupToGram(i.Item, i.Amount))
+		m := round(i.Amount * cupToMilliliter)
+		return fmt.Sprintf("%v %v / %v %v", g, gram, m, ml)
+	case ml:
+		c := round(1 / cupToMilliliter * i.Amount)
+		g := round(cupToGram(i.Item, c))
+		return fmt.Sprintf("%v %v / %v %v", c, cup, g, gram)
+	case tbsp:
+		c := round(1 / cupToTbsp * i.Amount)
+		g := cupToGram(i.Item, c)
+		return fmt.Sprintf("%v %v / %v %v", c, cup, g, gram)
+	case tl:
+		c := round(1 / cupToTsp * i.Amount)
+		g := cupToGram(i.Item, c)
+		return fmt.Sprintf("%v %v / %v %v", c, cup, g, gram)
+	default:
+		return ""
+	}
+}
+
+func gramToCup(item string, x float64) float64 {
+	if f, ok := convTable[item]; ok {
+		return x / 100 * f
+	}
+	return 0.0
+}
+
+func cupToGram(item string, x float64) float64 {
+	if f, ok := convTable[item]; ok {
+		return x * 1 / f * 100
+	}
+	return 0.0
+}
+
+func toTitle(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	newS := fmt.Sprint(strings.ToUpper(string(s[0])))
+	if len(s) > 1 {
+		newS += strings.ToLower(s[1:])
+	}
+	return newS
+}
+
+func round(f float64) float64 {
+	return math.Round(f*1000) / 1000
+}
---
+++ conversion_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGramToCup(t *testing.T) {
+	convTable["Quinoa"] = 0.555
+	i := Ingr{
+		Amount: 1,
+		Unit:   cup,
+		Item:   "Quinoa",
+		Notes:  "",
+	}
+	t.Log(i.uoms())
+	// want := fmt.Sprintf("0.2775 %v", gram)
+	// if got := i.uom(); got != want {
+	// 	t.Errorf("Error. Want: %v. Got: %v", want, got)
+	// }
+}
+
+func TestToTitle(t *testing.T) {
+	cases := []struct {
+		input, want string
+	}{
+		{"Test", "Test"},
+		{"test", "Test"},
+		{"TEST", "Test"},
+		{"", ""},
+		{"T", "T"},
+		{"t", "T"},
+	}
+	for i, c := range cases {
+		got := toTitle(c.input)
+		if got != c.want {
+			t.Errorf("Case %v failed. Want: %v, Got: %v", i, c.want, got)
+		}
+	}
+}
